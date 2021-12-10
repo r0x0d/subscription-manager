9d5169b8dc0d75c8069789e0b2cecc670f75da58
Christopher Snyder <csnyder@redhat.com>
2021-12-10 16:08:31 -0500

    WORKSPACE defaults to the root of the project checkout

--- jenkins/python3-stylish-tests.sh
+++ jenkins/python3-stylish-tests.sh
@@ -13,7 +13,7 @@
 #   we need to make /etc/pki/product and /etc/pki/entitlement
 
 echo "GIT_COMMIT:" "${GIT_COMMIT}"
-
+WORKSPACE=${WORKSPACE:-"`git rev-parse --show-toplevel`"}
 cd $WORKSPACE
 
 sudo yum clean expire-cache
